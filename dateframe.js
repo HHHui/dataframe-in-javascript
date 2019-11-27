351fa0c09b800232da2fd4309c14fc8fb1f2e63b
zohar <zohar.zhang@zoom.us>
2019-11-27 14:37:33 +0800

    finish group operator

---
+++ dateframe.js
@@ -0,0 +1,69 @@
+const data = [
+    {pId: 'P1', pName: 'PenDrive', quantity: 50,  country: 'US'},
+    {pId: 'P1', pName: 'PenDrive', quantity: 100,  country: 'UK'},
+    {pId: 'P2', pName: 'Mouse',    quantity: 100, country: 'UK'},
+    {pId: 'P3', pName: 'KeyBoard', quantity: 250, country: 'US'},
+    {pId: 'P1', pName: 'PenDrive', quantity: 300, country: 'US'},
+    {pId: 'P2', pName: 'Mouse',    quantity: 450, country: 'UK'},
+    {pId: 'P5', pName: 'Dvd',      quantity: 50,  country: 'UAE'}
+];
+
+const target = [
+    { pName: 'Mouse', UK: 550, US: 0}
+];
+
+
+// [ Row, Row, Row ]
+class DataFrame {
+    constructor(data) {
+        this.data = data;
+    }
+
+    groupBy(column) {
+        const g = this.data.reduce((g, row) => {
+            if(g[row[column]]) {
+                g[row[column]].push(row);
+            } else {
+                g[row[column]] = [row];
+            }
+            return g;
+        }, {});
+
+        const g2 = Object.entries(g).map(([colValue, rows])=>
+            ({ [column]: rows[0][column], rows })
+        );
+
+        return new GroupDataFrame(g2);
+    }
+
+    groupBys(columns) {
+        for (let column of columns) {
+            const g = this.groupBy(column);
+        }
+    }
+}
+
+// [
+//   { pId: 'P1', rows: [Row, Row, Row] }
+//   { pId: 'P2', rows: [Row, Row, Row] }
+// ]
+class GroupDataFrame {
+    constructor(gData) {
+        this.gData = gData;
+    }
+
+    // return GroupDataFrame
+    // [
+    //   { pId: 'P1', country: 'US', rows: [Row] }
+    //   { pId: 'P1', country: 'UK', rows: [Row, Row] }
+    // ]
+    groupBy(column) {
+        const gData = this.gData.flatMap(gRow => {
+            const { rows, ...rest } = gRow;
+            return new DataFrame(rows).groupBy(column).gData.map(gRow => ({ ...rest, ...gRow }));
+        });
+        return new GroupDataFrame(gData);
+    }
+}
+
+console.log(new DataFrame(data).groupBy('pId').groupBy('country').gData );
